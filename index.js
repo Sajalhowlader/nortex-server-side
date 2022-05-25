cfe6b310c8b1e0e65b989ea55f594fa96ca0d6b3
Sajalhowlader <sajolhowlader08@gmail.com>
2022-05-25 17:28:28 +0600

    add id for finding one tools

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ const port = process.env.PORT || 5000;
 // Middle Ware
 app.use(cors());
 app.use(express.json());
-// p 3EmyINqgmIuxsZJc   nortexTools
+
 // Root end point
 app.get("/", async (req, res) => {
   res.send("Server is connect");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://nortexTools:<password>@cluster0.vrkwl.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrkwl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -26,6 +25,19 @@ const run = async () => {
   try {
     await client.connect();
     console.log("Connect");
+    const toolsCollection = client.db("nortexTools").collection("tools");
+
+    app.get("/tools", async (req, res) => {
+      const tools = await toolsCollection.find({}).toArray();
+      res.send(tools);
+    });
+
+    app.get("/tools/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const tool = await toolsCollection.findOne(query);
+      res.send(tool);
+    });
   } finally {
   }
 };
